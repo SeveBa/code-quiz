d283f48dbaa38d86a8e527509bc60c5d4165d9ac
Seve Barrutia <sevebarrutia@me.com>
2021-04-17 21:44:23 -0500

    pseudo code added to js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Query selectors
+
+// Global Variable Declarations
+
+// Upon start button (Quiz Init)
+
+// Time Counter is initiated
+
+// Question 1 is presented 
+
+// If answer is correct, "Correct" indication is presented 
+
+// Score is added 
+
+// User is moved to next question
+
+// If incorrect, "Wrong" indication is presented
+
+// Time is subtracted from Time Counter
+
+// User is moved to next question
+
+// Game is over when all questions are answered
+
+// Game is over when Time Counter === 0
+
+// User can input initials and game score is saved
