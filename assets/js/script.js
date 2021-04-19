5f2d789654634a87297222016a271ad92d4e6dea
Seve Barrutia <sevebarrutia@me.com>
2021-04-18 22:11:56 -0500

    what?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 // Query selectors
+var btn = window.document.querySelector(".btn");
+// console.dir(btn);
 
 // Global Variable Declarations
+const button = document.querySelector('button');
+let timerel = document.getElementById("timer");
+
+
+
 
 // Upon start button (Quiz Init)
 
 // Time Counter is initiated
 
-// Question 1 is presented 
+
+
+// Question(s) are presented 
 
 // If answer is correct, "Correct" indication is presented 
 
@@ -25,3 +34,7 @@
 // Game is over when Time Counter === 0
 
 // User can input initials and game score is saved
+
+button.addEventListener('click', event => {
+   
+  });
